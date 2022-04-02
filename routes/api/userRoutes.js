f3e32f5938a101a9ad32595b3515f8a44fc458a6
Sean Ashby <sean.r.ashby@gmail.com>
2022-04-02 12:07:38 -0700

    adding routes and utilities

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+
+const {
+    getUsers,
+    addUser,
+    getUserByID,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/user-controller');
+
+
+router.route('/')
+.get(getUsers)
+.post(addUser);
+
+
+router.route('/:id')
+.get(getUserByID)
+.put(updateUser)
+.delete(deleteUser);
+
+
+
+router.route('/:id/friends/:friendID')
+.post(addFriend)
+.delete(deleteFriend)
+
+
+module.exports = router;
