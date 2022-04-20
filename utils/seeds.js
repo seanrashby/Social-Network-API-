c789b1779c5472c8651c35f6aa5b49c3ae66331a
Sean Ashby <sean.r.ashby@gmail.com>
2022-04-19 18:24:14 -0700

    updating seeds

--- utils/seeds.js
+++ utils/seeds.js
@@ -2,3 +2,10 @@ const connection = require('../config/connection');
 const { Post, User } = require('../models');
 const getRandomName = require('./data');
 
+console.log(getRandomName());
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+  await Post.deleteMany({});
+  await User.deleteMany({});
