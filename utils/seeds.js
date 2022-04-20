89528908724adb5dc082440f9a538ce1dcf9f44b
Sean Ashby <sean.r.ashby@gmail.com>
2022-04-19 18:56:12 -0700

    updating data

--- utils/seeds.js
+++ utils/seeds.js
@@ -1,30 +1,41 @@
 const connection = require('../config/connection');
-const { Post, User } = require('../models');
-const getRandomName = require('./data');
+const { Thought, User } = require('../models');
+const { users, thoughts } = require('./data.json');
 
-console.log(getRandomName());
+
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
-  console.log('connected');
-  await Post.deleteMany({});
+  console.log('Access Granted');
+
+  
   await User.deleteMany({});
 
-  const users = [];
-
-  for (let i = 0; i < 20; i++) {
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-
-    users.push({
-        first,
-        last,
-        age: Math.floor(Math.random() * (99 - 18 + 1) + 18),
-      });
-    }
   
-    await User.collection.insertMany(users);
-    console.log(users);
-    process.exit(0);
-  });
+  await Thought.deleteMany({});
+
+
+
+  
+  await User.collection.insertMany(users);
+
+  
+  await Thought.collection.insertMany(thoughts);
+
+ 
+  await User.collection.findOneAndUpdate(
+    { _id: users[0]._id },
+    { $addToSet: { followers: users[1]._id, thoughts: thoughts[0]._id } }
+  );
+  await User.collection.findOneAndUpdate(
+    { _id: users[1]._id },
+    { $addToSet: { followers: users[0]._id, thoughts: thoughts[1]._id } }
+  );
+
+
+ 
+  console.table(users);
+  console.table(thoughts);
+  console.info('Data Seeded');
+  process.exit(0);
+});
