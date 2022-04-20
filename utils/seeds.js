75f836d7d5f4e3d69bdfe2e032e181f68fe6da50
Sean Ashby <sean.r.ashby@gmail.com>
2022-04-19 18:25:13 -0700

    updating logic in seeds

--- utils/seeds.js
+++ utils/seeds.js
@@ -16,3 +16,15 @@ connection.once('open', async () => {
     const fullName = getRandomName();
     const first = fullName.split(' ')[0];
     const last = fullName.split(' ')[1];
+
+    users.push({
+        first,
+        last,
+        age: Math.floor(Math.random() * (99 - 18 + 1) + 18),
+      });
+    }
+  
+    await User.collection.insertMany(users);
+    console.log(users);
+    process.exit(0);
+  });
